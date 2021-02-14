198b26b39b2933b9d52add5a5ea52a4d895a7748
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2021-02-13 21:11:47 -0300

    Finishing Sum numbers list

--- module01/sum.go
+++ module01/sum.go
@@ -3,5 +3,16 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	return 0
+	//Structured programming solution
+	//total := 0
+	//for _, num := range numbers {
+	//	total += num
+	//}
+	//return total
+
+	//Solution using recursion
+	if len(numbers) == 0 {
+		return 0
+	}
+	return numbers[0] + Sum(numbers[1:])
 }
