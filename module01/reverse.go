ffdf47922f5b7c567a386baa341dc794afd09e2d
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2021-02-13 21:28:53 -0300

    Reverse string with String Builder

--- module01/reverse.go
+++ module01/reverse.go
@@ -1,5 +1,7 @@
 package module01
 
+import "strings"
+
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
@@ -7,9 +9,16 @@ package module01
 //   Reverse("alphabet") => "tebahpla"
 //
 func Reverse(word string) string {
-	reversed := make([]rune, len(word))
-	for i := len(word) - 1; i > -1; i-- {
-		reversed[len(word) - i - 1] = rune(word[i])
+	//reversed := make([]rune, len(word))
+	//for i := len(word) - 1; i > -1; i-- {
+	//	reversed[len(word) - i - 1] = rune(word[i])
+	//}
+	//return string(reversed)
+
+	//Using String Builder
+	var sb strings.Builder
+	for i := len(word)-1; i >= 0; i-- {
+		sb.WriteByte(word[i])
 	}
-	return string(reversed)
+	return sb.String()
 }
